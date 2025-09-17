1c9f658217853891c45f6170978423895ef81a13
yadav-sudhanshu <66832329+yadav-sudhanshu@users.noreply.github.com>
2025-09-17 12:26:48 +0530

    first commit

---
+++ src/test/java/com/Rate_limiter/Ratelimiter/RatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rate_limiter.Ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
